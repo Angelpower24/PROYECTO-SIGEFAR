9a5f4c3d71fae4e4c295f119386b2c2a3e2b3ec4
Angelpower24 <angel.floress@istpargentina.edu.pe>
2026-07-15 08:07:37 -0500

    Commint 3: Se completo el archivo "logger.py"

---
+++ config/logger.py
@@ -0,0 +1,28 @@
+import datetime
+
+# LOGGER (Singleton)
+
+class Logger:
+    _instancia = None
+    def __new__(cls):
+        if cls._instancia is None:
+            cls._instancia = super().__new__(cls)
+            cls._instancia._logs = []
+        return cls._instancia
+    def _registrar(self, nivel, mensaje):
+        hora = datetime.datetime.now().strftime("%H:%M:%S")
+        entrada = {"hora": hora,"nivel": nivel,"msg": mensaje}
+        self._logs.append(entrada)
+
+    def info(self, msg):self._registrar("INFO", msg)
+    def warning(self, msg):self._registrar("WARNING", msg)
+    def error(self, msg):self._registrar("ERROR", msg)
+    
+    def mostrar_logs(self):
+        print(f"\n======= HISTORIAL REGISTRADO ({len(self._logs)} eventos) =======")
+        for log in self._logs:
+            print(f"[{log['hora']}] {log['nivel']:8} | {log['msg']}")
+
+    def limpiar(self):
+        self._logs.clear()
+        print("Historial limpio.")
