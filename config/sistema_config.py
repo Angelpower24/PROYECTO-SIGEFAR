819300597606731e58793c08737d06833052ab08
Angelpower24 <angel.floress@istpargentina.edu.pe>
2026-07-22 15:05:30 -0500

    en sistema_config.py se modificó para que se vea un poco más ordenado al momento de mostrar eventos del sistema

--- config/sistema_config.py
+++ config/sistema_config.py
@@ -12,5 +12,10 @@ class SistemaConfig:
             cls._instancia.empresa = "Instituto de Educación Superior Tecnológico Público ARGENTINA "
             cls._instancia.autor = "Angel Flores"
             Logger().info(
-                f"Sistema iniciado: {cls._instancia.nombre} Version :{cls._instancia.version} Empresa :{cls._instancia.empresa} Autor :{cls._instancia.autor}")
+                f"Sistema iniciado\n"
+                f"Nombre   : {cls._instancia.nombre}\n"
+                f"Versión  : {cls._instancia.version}\n"
+                f"Empresa  : {cls._instancia.empresa}\n"
+                f"Autor    : {cls._instancia.autor}"
+                )
         return cls._instancia
