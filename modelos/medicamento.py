4c73cc85b5d5e99b9724d56145ce2f3ddd845501
Angelpower24 <angel.floress@istpargentina.edu.pe>
2026-07-22 15:18:16 -0500

    en medicamento.py se corrigió el mismo error del atributo, ya que estaba como "id" y tenia que ser "id_medicamento"

--- modelos/medicamento.py
+++ modelos/medicamento.py
@@ -2,18 +2,18 @@
 
 class Medicamento:
     def __init__(self, nomb_med, precio, stock):
-        self.id = None
+        self.id_medicamento = None
         self.nomb_med = nomb_med
         self.precio = precio
         self.stock = stock
 
     def __str__(self):
-        return f"[{self.id}] {self.nomb_med} | Precio: S/. {self.precio:.2f} | Stock: {self.stock}"
+        return f"[{self.id_medicamento}] {self.nomb_med} | Precio: S/. {self.precio:.2f} | Stock: {self.stock}"
     
     
     def to_dict(self):
         return {
-            "id": self.id,
+            "id_medicamento": self.id_medicamento,
             "nomb_med": self.nomb_med,
             "precio": self.precio,
             "stock": self.stock,
@@ -22,5 +22,5 @@ class Medicamento:
     @classmethod
     def from_dict(cls,datos):
         m = cls(datos["nomb_med"], datos["precio"], datos["stock"])
-        m.id = datos["id"]
+        m.id_medicamento = datos["id_medicamento"]
         return m
