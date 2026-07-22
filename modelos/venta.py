f6f4931d12d362ca7178043ee6aec7a76c57e7b7
Angelpower24 <angel.floress@istpargentina.edu.pe>
2026-07-22 15:26:51 -0500

    en venta.py tambien hubo error de atributo, se corrigió "id" por "id_medicamento"

--- modelos/venta.py
+++ modelos/venta.py
@@ -1,26 +1,28 @@
 # CLASE VENTA
 
 class Venta:
-    def __init__(self, fecha_venta, id_cliente, id_medicamento):
-        self.id = None
-        self.fecha_venta    = fecha_venta
-        self.id_cliente     = id_cliente
+
+    def __init__(self, id_cliente, id_medicamento):
+        self.id_venta = None
+        self.fecha_venta = None
+        self.id_cliente = id_cliente
         self.id_medicamento = id_medicamento
 
     def __str__(self):
-        return f"[{self.id}] Fecha: {self.fecha_venta} | Cliente ID: {self.id_cliente} | Medicamento ID: {self.id_medicamento}"
-    
-    
+        return f"[{self.id_venta}] Fecha: {self.fecha_venta} | Cliente ID: {self.id_cliente} | Medicamento ID: {self.id_medicamento}"
+
     def to_dict(self):
         return {
-            "id": self.id,
+            "id_venta": self.id_venta,
             "fecha_venta": self.fecha_venta,
             "id_cliente": self.id_cliente,
-            "id_medicamento": self.id_medicamento,
+            "id_medicamento": self.id_medicamento
         }
-        
+
     @classmethod
-    def from_dict(cls,datos):
-        v = cls(datos["fecha_venta"], datos["id_cliente"], datos["id_medicamento"])
-        v.id = datos["id"]
+    def from_dict(cls, datos):
+        v = cls(datos["id_cliente"], datos["id_medicamento"])
+        v.id_venta = datos["id_venta"]
+        v.fecha_venta = datos["fecha_venta"]
         return v
+    
