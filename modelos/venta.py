0273aba60271071d734e709b748ce08db7fe1be4
Angelpower24 <angel.floress@istpargentina.edu.pe>
2026-07-23 18:20:28 -0500

    Commit 26: Se agregó en la "clase venta" los cambios correspondientes en base a los campos "cantidad" y "total"

--- modelos/venta.py
+++ modelos/venta.py
@@ -1,27 +1,30 @@
 # CLASE VENTA
-
 class Venta:
 
-    def __init__(self, id_cliente, id_medicamento):
+    def __init__(self, id_cliente, id_medicamento, cantidad, total):
         self.id_venta = None
         self.fecha_venta = None
         self.id_cliente = id_cliente
         self.id_medicamento = id_medicamento
+        self.cantidad = cantidad
+        self.total = total
 
     def __str__(self):
-        return f"[{self.id_venta}] Fecha: {self.fecha_venta} | Cliente ID: {self.id_cliente} | Medicamento ID: {self.id_medicamento}"
+        return f"[{self.id_venta}] Fecha: {self.fecha_venta} | Cliente ID: {self.id_cliente} | Medicamento ID: {self.id_medicamento} | Cantidad: {self.cantidad} | Total: {self.total}"
 
     def to_dict(self):
         return {
             "id_venta": self.id_venta,
             "fecha_venta": self.fecha_venta,
             "id_cliente": self.id_cliente,
-            "id_medicamento": self.id_medicamento
+            "id_medicamento": self.id_medicamento,
+            "cantidad": self.cantidad,
+            "total": self.total
         }
 
     @classmethod
     def from_dict(cls, datos):
-        v = cls(datos["id_cliente"], datos["id_medicamento"])
+        v = cls(datos["id_cliente"], datos["id_medicamento"], datos["cantidad"], datos["total"])
         v.id_venta = datos["id_venta"]
         v.fecha_venta = datos["fecha_venta"]
         return v
